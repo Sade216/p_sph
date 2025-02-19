7e89168fe262c7777c7ab1bf51f85dec0a51f77b
sade216 <vlad.tok.1997@mail.ru>
2025-02-20 02:54:40 +0700

    fix error page

--- frontend/src/components/pages/Error/ErrorPage.tsx
+++ frontend/src/components/pages/Error/ErrorPage.tsx
@@ -5,7 +5,7 @@ function ErrorPage() {
   return (
     <div className='flex flex-col justify-center items-center my-5'>
         <div className='text-7xl'>Error 404</div>
-        <div className='mt-5 text-2xl'>Такая страница не найдена</div>
+        <div className='mt-5 text-2xl'>Cтраница не найдена</div>
         <NavLink 
             to='/' 
             className='text-purple-600 hover:text-purple-400 mt-10 text-l'
